4223a521fd68e2a594b2e4fbbd2c2398db870628
Rares Croicia <rarescroicia@gmail.com>
2023-10-20 12:30:19 +0300

    Add lab01
    Signed-off-by: Rares Croicia <rarescroicia@gmail.com>

--- laboratoare/lab01/add_parallel.c
+++ laboratoare/lab01/add_parallel.c
@@ -1,20 +1,38 @@
+#include <time.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
+#define min(X,Y) (((X) < (Y)) ? (X) : (Y))
 /*
     schelet pentru exercitiul 5
 */
 
+typedef struct {
+  int start;
+  int end;
+} thread_arg_t;
+
 int *arr;
 int array_size;
 int num_threads;
+
+void *f(void *arg) {
+  thread_arg_t *thread_arg = (thread_arg_t *)arg;
+  for (int i = thread_arg->start; i < thread_arg->end; i++) {
+    arr[i] += 100;
+  }
+  pthread_exit(NULL);
+}
 
 int main(int argc, char *argv[]) {
   if (argc < 3) {
     fprintf(stderr, "Specificati dimensiunea array-ului si numarul de thread-uri\n");
     exit(-1);
   }
+  struct timespec start, finish; 
+  double elapsed; 
 
   array_size = atoi(argv[1]);
   num_threads = atoi(argv[2]);
@@ -24,28 +42,56 @@ int main(int argc, char *argv[]) {
     arr[i] = i;
   }
 
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
+  // for (int i = 0; i < array_size; i++) {
+  //   printf("%d", arr[i]);
+  //   if (i != array_size - 1) {
+  //     printf(" ");
+  //   } else {
+  //     printf("\n");
+  //   }
+  // }
+
+  pthread_t threads[num_threads];
+  int r;
+  void *status;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+  for(int i = 0; i < num_threads; i++) {
+    thread_arg_t *arg = malloc(sizeof(thread_arg_t));
+    arg->start = i * (double) array_size / num_threads;
+    arg->end = min((i + 1) * (double)array_size / num_threads, array_size);
+    r = pthread_create(&threads[i], NULL, f, (void*)arg);
+    if (r) {
+      printf("Eroare la crearea thread-ului %d\n", i);
+      exit(-1);
     }
+    free(arg);
   }
 
-  // TODO: aceasta operatie va fi paralelizata cu num_threads fire de executie
-  for (int i = 0; i < array_size; i++) {
-    arr[i] += 100;
-  }
-
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
+  for (int i = 0; i < num_threads; i++) {
+    r = pthread_join(threads[i], &status);
+    if (r) {
+      printf("Eroare la asteptarea thread-ului %d\n", i);
+      exit(-1);
     }
   }
+  
+  clock_gettime(CLOCK_MONOTONIC, &finish);
+
+  elapsed = (finish.tv_sec - start.tv_sec);
+  elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+
+  printf("Timpul de executie paralel: %lf\n", elapsed);
+
+  // for (int i = 0; i < array_size; i++) {
+  //   printf("%d", arr[i]);
+  //   if (i != array_size - 1) {
+  //     printf(" ");
+  //   } else {
+  //     printf("\n");
+  //   }
+  // }
 
   return 0;
 }
--- laboratoare/lab01/add_serial.c
+++ laboratoare/lab01/add_serial.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
     implementare seriala pentru exercitiul 5
@@ -22,28 +23,37 @@ int main(int argc, char *argv[]) {
     arr[i] = i;
   }
 
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
-    }
-  }
+  // for (int i = 0; i < array_size; i++) {
+  //   printf("%d", arr[i]);
+  //   if (i != array_size - 1) {
+  //     printf(" ");
+  //   } else {
+  //     printf("\n");
+  //   }
+  // }
 
+  struct timespec start, finish; 
+  double elapsed;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
   // Vom folosi varianta implementata serial ca referinta (baseline) pentru calculul acceleratiei (speedup-ului)
   for (int i = 0; i < array_size; i++) {
     arr[i] += 100;
   }
 
-  for (int i = 0; i < array_size; i++) {
-    printf("%d", arr[i]);
-    if (i != array_size - 1) {
-      printf(" ");
-    } else {
-      printf("\n");
-    }
-  }
+  clock_gettime(CLOCK_MONOTONIC, &finish);
+
+  elapsed = (finish.tv_sec - start.tv_sec);
+  elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+  printf("Timpul de executie pentru implementarea seriala este %lf secunde.\n", elapsed);
+  // for (int i = 0; i < array_size; i++) {
+  //   printf("%d", arr[i]);
+  //   if (i != array_size - 1) {
+  //     printf(" ");
+  //   } else {
+  //     printf("\n");
+  //   }
+  // }
 
   return 0;
 }
