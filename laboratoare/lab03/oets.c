abeb5f31bf11dcd8e525b6f7fc1159dc8289522b
Rares Croicia <rarescroicia@gmail.com>
2023-11-01 16:12:12 +0200

    Add lab3
    Signed-off-by: Rares Croicia <rarescroicia@gmail.com>

--- laboratoare/lab03/oets.c
+++ laboratoare/lab03/oets.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
+#include <time.h>
 
 int N;
 int P;
 int *v;
 int *vQSort;
+pthread_barrier_t barrier;
 
 void compare_vectors(int *a, int *b) {
 	int i;
@@ -60,7 +62,7 @@ void init()
 		exit(1);
 	}
 
-	srand(42);
+	srand(time(NULL));
 
 	for (i = 0; i < N; i++)
 		v[i] = rand() % N;
@@ -75,12 +77,36 @@ void print()
 	compare_vectors(v, vQSort);
 }
 
+
+/*
+index	0 1 2 3 4 5 6 7 8 9
+thread	0 x 1 x 2 x 3 x 0 x		
+thread  x 0 x 1 x 2 x 3 x 0
+*/
+
 void *thread_function(void *arg)
 {
 	int thread_id = *(int *)arg;
-
-	// TODO: implementati aici OETS paralel
-
+	int start = thread_id * 2;
+	// OETS
+	for(int i = 0; i < N; i++) {
+		for(int j = start; j < N - 1; j += 2 * P) {
+			if(v[j] > v[j + 1]) {
+				int aux = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = aux;
+			}
+		}
+		pthread_barrier_wait(&barrier);
+		for(int j = start + 1; j < N - 1; j += 2 * P) {
+			if(v[j] > v[j + 1]) {
+				int aux = v[j];
+				v[j] = v[j + 1];
+				v[j + 1] = aux;
+			}
+		}
+		pthread_barrier_wait(&barrier);
+	}
 	pthread_exit(NULL);
 }
 
@@ -97,7 +123,7 @@ int main(int argc, char *argv[])
 	for (i = 0; i < N; i++)
 		vQSort[i] = v[i];
 	qsort(vQSort, N, sizeof(int), cmp);
-
+	pthread_barrier_init(&barrier, NULL, P);	
 	// se creeaza thread-urile
 	for (i = 0; i < P; i++) {
 		thread_id[i] = i;
@@ -108,21 +134,22 @@ int main(int argc, char *argv[])
 	for (i = 0; i < P; i++) {
 		pthread_join(tid[i], NULL);
 	}
+	pthread_barrier_destroy(&barrier);
 
 	// bubble sort clasic - trebuie transformat in OETS si paralelizat
-	int sorted = 0;
-	while (!sorted) {
-		sorted = 1;
+	// int sorted = 0;
+	// while (!sorted) {
+	// 	sorted = 1;
 
-		for (i = 0; i < N-1; i++) {
-			if(v[i] > v[i + 1]) {
-				aux = v[i];
-				v[i] = v[i + 1];
-				v[i + 1] = aux;
-				sorted = 0;
-			}
-		}
-	}
+	// 	for (i = 0; i < N-1; i++) {
+	// 		if(v[i] > v[i + 1]) {
+	// 			aux = v[i];
+	// 			v[i] = v[i + 1];
+	// 			v[i + 1] = aux;
+	// 			sorted = 0;
+	// 		}
+	// 	}
+	// }
 
 	// se afiseaza vectorul etalon
 	// se afiseaza vectorul curent
