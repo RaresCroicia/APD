6330b4868febf1a43072dd3057ab58b832753da4
Rares Croicia <rarescroicia@gmail.com>
2023-11-30 03:10:17 +0200

    S-a inceput tema 2, avem load balancer, dar nu avem noduri care sa ruleze =)))
    Signed-off-by: Rares Croicia <rarescroicia@gmail.com>

--- tema2/src/MyDispatcher.java
+++ tema2/src/MyDispatcher.java
@@ -4,11 +4,78 @@ import java.util.List;
 
 public class MyDispatcher extends Dispatcher {
 
+    int lastRoundRobinTask;
+    int numberOfHosts;
     public MyDispatcher(SchedulingAlgorithm algorithm, List<Host> hosts) {
         super(algorithm, hosts);
+        lastRoundRobinTask = -1;
+        numberOfHosts = hosts.size();
     }
 
+    private void addTaskRoundRobin(Task task) {
+        int hostIndex = (lastRoundRobinTask + 1) % numberOfHosts;
+        lastRoundRobinTask ++;
+        hosts.get(hostIndex).addTask(task);
+    }
+
+    private void addTaskShortestQueue(Task task) {
+        int minQueueSize = Integer.MAX_VALUE;
+        int minQueueIndex = -1;
+        for (int i = 0; i < numberOfHosts; i++) {
+            int queueSize = hosts.get(i).getQueueSize();
+            if (queueSize < minQueueSize) {
+                minQueueSize = queueSize;
+                minQueueIndex = i;
+            }
+        }
+        hosts.get(minQueueIndex).addTask(task);
+    }
+
+    private void addTaskSITA(Task task) {
+        switch(task.getType()) {
+            case SHORT:
+                hosts.get(0).addTask(task);
+                break;
+            case MEDIUM:
+                hosts.get(1).addTask(task);
+                break;
+            case LONG:
+                hosts.get(2).addTask(task);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addTaskLWL(Task task) {
+        long minWorkLeft = Integer.MAX_VALUE;
+        int minWorkLeftIndex = -1;
+        for (int i = 0; i < numberOfHosts; i++) {
+            long workLeft = hosts.get(i).getWorkLeft();
+            if (workLeft < minWorkLeft) {
+                minWorkLeft = workLeft;
+                minWorkLeftIndex = i;
+            }
+        }
+        hosts.get(minWorkLeftIndex).addTask(task);
+    }
     @Override
     public void addTask(Task task) {
+        switch(algorithm) {
+            case ROUND_ROBIN:
+                addTaskRoundRobin(task);
+                break;
+            case SHORTEST_QUEUE:
+                addTaskShortestQueue(task);
+                break;
+            case SIZE_INTERVAL_TASK_ASSIGNMENT:
+                addTaskSITA(task);
+                break;
+            case LEAST_WORK_LEFT:
+                addTaskLWL(task);
+                break;
+            default:
+                break;
+        }
     }
 }
--- tema2/src/MyHost.java
+++ tema2/src/MyHost.java
@@ -1,25 +1,58 @@
 /* Implement this class. */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MyHost extends Host {
+    private ArrayList<Task> tasks;
+    private HashMap<Integer, Long> workTimeLeft;
+    private boolean isRunning = true;
     @Override
     public void run() {
+        while(isRunning) {
+            try {
+                sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            // TODO: implementeaza logica de rulare cu preemtie si prioritati si trecere a timpului
+        }
     }
 
     @Override
     public void addTask(Task task) {
+        // If tasks is not initialized, initialize it
+        if (tasks == null) {
+            tasks = new ArrayList<>();
+        }
+        tasks.add(task);
+        if (workTimeLeft == null) {
+            workTimeLeft = new HashMap<>();
+        }
+        workTimeLeft.put(task.getId(), task.getLeft());
     }
 
     @Override
     public int getQueueSize() {
-        return 0;
+        return tasks == null ? 0 : tasks.size();
     }
 
     @Override
     public long getWorkLeft() {
-        return 0;
+        long workLeft = 0;
+        for (Task task : tasks) {
+            workLeft += workTimeLeft.get(task.getId());
+        }
+        return workLeft;
     }
 
     @Override
     public void shutdown() {
+        for (Task task : tasks) {
+            if(task.getFinish() == 0)
+                task.finish();
+        }
+        isRunning = false;
     }
 }
